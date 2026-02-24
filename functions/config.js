f30549f772519fba6f5a3af91d9d5795208239c4
bktime <wisasen@gmail.com>
2026-02-24 15:52:45 +0700

    Update config.js

--- functions/config.js
+++ functions/config.js
@@ -4,7 +4,7 @@ export async function onRequest(context) {
 
     OAuth_gg_id: context.env.VITE_OAuth_gg_id || "",
 
-    telegramEnabled: context.env.VITE_token_telegram || "",
+    telegram_bot_token: context.env.VITE_token_telegram || "",
 
     botName: context.env.VITE_botName_telegram || "",
 
