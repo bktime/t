d1913af1484c09ea27705641b6dbc762a07ff55a
bktime <wisasen@gmail.com>
2026-03-09 11:11:50 +0700

    Update upload-signature.js

--- functions/upload-signature.js
+++ functions/upload-signature.js
@@ -1,19 +1,24 @@
-export async function onRequest(context) {
+export async function onRequest(context){
 
- const timestamp = Math.floor(Date.now() / 1000);
+ const timestamp = Math.floor(Date.now()/1000);
 
  const cloudName = context.env.VITE_CLOUDINARY_NAME;
  const apiKey = context.env.VITE_CLOUDINARY_API;
  const apiSecret = context.env.VITE_CLOUDINARY_SECRET;
 
- const params = `timestamp=${timestamp}`;
+ const public_id = "signature/user-signature";
+ const folder = "signature";
+ const overwrite = "true";
+
+ const params =
+  `folder=${folder}&overwrite=${overwrite}&public_id=${public_id}&timestamp=${timestamp}`;
 
  const data = new TextEncoder().encode(params + apiSecret);
 
  const hash = await crypto.subtle.digest("SHA-1", data);
 
  const signature = [...new Uint8Array(hash)]
-  .map(b => b.toString(16).padStart(2, "0"))
+  .map(b => b.toString(16).padStart(2,"0"))
   .join("");
 
  return new Response(
@@ -21,12 +26,15 @@ export async function onRequest(context) {
    timestamp,
    signature,
    apiKey,
-   cloudName
+   cloudName,
+   public_id,
+   folder,
+   overwrite
   }),
   {
-   headers: {
-    "Content-Type": "application/json",
-    "Cache-Control": "no-store"
+   headers:{
+    "Content-Type":"application/json",
+    "Cache-Control":"no-store"
    }
   }
  );
